46edac1f468f91cda729ac2997c250074e409214
aragulkumar <aragulkumar11@gmail.com>
2025-09-26 23:49:14 +0530

    scanner class

---
+++ Java_Program/Scannerclass.java
@@ -0,0 +1,17 @@
+package Java_Program;
+import java.util.Scanner;
+
+
+
+
+public class Scannerclass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            System.out.println(sc.next());
+        }
+        sc.close();
+    }
+
+    
+}
