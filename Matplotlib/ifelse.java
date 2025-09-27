0f7ba378b3ed49555fb0d880ba57d00f4b28b1d5
aragulkumar <aragulkumar11@gmail.com>
2025-09-27 21:14:14 +0530

    if else java

---
+++ Matplotlib/ifelse.java
@@ -0,0 +1,12 @@
+package Matplotlib;
+
+public class ifelse {
+    public static void main(String[] args) {
+        int a = 10;
+        if (a > 0) {
+            System.out.println("Positive number");
+        } else {
+            System.out.println("Non-positive number");
+        }
+    }
+}
